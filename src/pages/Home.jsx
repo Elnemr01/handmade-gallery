f6d372e0f977cacba4f3e4f5cc77b20bc02130d1
Ahmed Elnemr <159473015+Elnemr01@users.noreply.github.com>
2025-05-19 23:42:50 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
       {/* Hero Section */}
       <section className="py-20 bg-gradient-to-br from-gray-800 to-rose-900">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex flex-col md:flex-row items-center">
+          <div className="flex flex-col md:flex-row items-center gap-10">
             {/* Right side - Image */}
             <div className="md:w-1/2 animate-fade-in-up">
               <img
